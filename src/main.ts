d8802dc0744015c476aabf4cd9cbc76f45b951fd
Max <maximrimkus@yandex.by>
2023-12-17 12:16:36 +0300

    Added base greeting view

--- src/main.ts
+++ src/main.ts
@@ -1,21 +1,24 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+
 import PrimeVue from 'primevue/config'
+import ToastService from 'primevue/toastservice'
+import Button from 'primevue/button'
 
 import App from './App.vue'
 import router from './router'
 
-import ToastService from 'primevue/toastservice'
-
 import './assets/main.css'
 import 'primevue/resources/themes/saga-blue/theme.css'
 import 'primevue/resources/primevue.min.css'
-import 'primeicons/primeicons.css' 
+import 'primeicons/primeicons.css'
+import '/node_modules/primeflex/primeflex.css'
 
 const app = createApp(App)
 
 app.use(PrimeVue)
 app.use(ToastService)
+app.component('Button', Button)
 
 app.use(createPinia())
 app.use(router)
